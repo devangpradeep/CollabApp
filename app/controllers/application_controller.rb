d72e5fbf70f3c32eeba60fd267c2fecfe0d41298
Devang Pradeep <devang.pradeep@codecraft.co.in>
2023-11-08 10:25:19 +0530

    Create UI to add new posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def redirect_if_not_signed_in
+        redirect_to_root_path if !user_signed_in?
+    end
+
+    def redirect_if_signed_in
+        redirect_to_root_path if user_signed_in?
+    end
 end
